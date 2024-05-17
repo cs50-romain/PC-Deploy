19c1e8e09c2f92f18651eab886a81bba507e7320
cs50-romain <cs50.git.romain@gmail.com>
2024-05-16 21:51:35 -0400

    read/write from/to json files for clientstruct

--- cmd/controlcenter/client.go
+++ cmd/controlcenter/client.go
@@ -2,15 +2,17 @@ package controlcenter
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"os"
 	"strings"
 )
 
 type Client struct {
-	Name		string
-	Locations	[]string
-	Automate	bool
-	ConfigFile	*ClientConfig
+	Name		string `json:"Name"`
+	Locations	[]string `json:"Locations"`
+	Automate	bool `json:"Automate"`
+	ConfigFile	ClientConfig
 }
 
 type ClientConfig struct {
@@ -27,7 +29,7 @@ func CreateClient(name, locations string, configFile *ClientConfig) *Client {
 		Name: name,
 		Locations: strings.Split(locations, ", "),
 		Automate: true,
-		ConfigFile: configFile,
+		ConfigFile: *configFile,
 	}
 }
 
@@ -57,4 +59,41 @@ func (c *Client) SaveFile(w io.Writer) error {
 	}
 
 	return nil
+}
+
+func getClientFromFile(filename string) (*Client, error) {
+	// Open file
+	filepath := "/home/lettuce/go/PCDeploy/storage/clients/" + filename
+	fmt.Println(filename, filepath)
+	byt, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	var client Client
+	if err = json.Unmarshal(byt, &client); err != nil {
+		return nil, err
+	}
+
+	return &client, nil
+}
+
+func InitClients() (map[string]Client, error) {
+	var cls = make(map[string]Client)
+
+	// Read directory where all clients.json 
+	dirs, err := os.ReadDir("/home/lettuce/go/PCDeploy/storage/clients/")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dir := range dirs {
+		client, err := getClientFromFile(dir.Name())
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		cls[client.Name] = *client
+	}
+	return cls, nil
 }
