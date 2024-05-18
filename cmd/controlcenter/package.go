ff39bc3122e1a0b9189e9621268a7b0a86d0b0c1
cs50-romain <cs50.git.romain@gmail.com>
2024-05-18 18:28:28 -0400

    init package struct

--- cmd/controlcenter/package.go
+++ cmd/controlcenter/package.go
@@ -1,7 +1,41 @@
 package controlcenter
 
-import (
-	"encoding/xml"
-)
+import "encoding/xml"
+
+//"encoding/xml"
 
 // CREATE FUNCTION NAMES AND STRUCTS BEFORE ANYTHING
+type Package struct {
+	ComputerName	string
+	timeZone	string
+	localAdmin	string
+	localLolz	string
+	Wifi		bool
+	sSID		string
+	sSIDLolz	string
+}
+
+func NewPackage(computerName, timeZone string) *Package {
+	return &Package{
+		ComputerName: computerName,
+		timeZone: timeZone,
+	}
+}
+
+func (p *Package) SetLocalAdmin(username, lolz string) {
+	p.localAdmin = username
+	p.localLolz = lolz
+}
+
+func (p *Package) SetWifi(ssid, lolz string) {
+	p.sSID = ssid
+	p.sSIDLolz = lolz
+}
+
+func (p *Package) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	return nil
+}
+
+func (p *Package) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return nil
+}
